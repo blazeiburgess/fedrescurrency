3df1680dfc93a0dfde817ba4e9fcb4a6ff03ea0d
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-17 12:21:36 -0500

    Add method to sanitize pulled data, debuged relative xpaths, began pulling

--- foreignexchangerates/foreignexchangerates/spiders/federalreserve.py
+++ foreignexchangerates/foreignexchangerates/spiders/federalreserve.py
@@ -3,19 +3,22 @@ import scrapy
 
 class FederalreserveSpider(scrapy.Spider):
     name = "federalreserve"
-    allowed_domains = ["federalreserve.gov"]
-    start_urls = ['http://federalreserve.gov/']
+    # allowed_domains = ["federalreserve.gov"]
+    start_urls = ['https://www.federalreserve.gov/releases/h10/hist/']
+
+    def clean_data(self, txt):
+        return txt.replace('  ', '')
 
     def parse(self, response):
-        for url in response.xpath('//table[@class="statistics"]/tbody/tr/th/a/@href'):
+        for href in response.xpath('//table[@class="statistics"]/tr/th/a/@href').extract():
             yield scrapy.Request(response.urljoin(href), callback=self.parse_rates)
             
     def parse_rates(self, response):
         for rate in response.xpath('//table[@class="statistics"]/tr'):
             yield {
-                "country": response.xpath('//h3/text()'),
-                "date": rate.xpath('//th/text()'),
-                "rate": rate.xpath('//td/text()')
+                "country": response.xpath('//h3/text()').extract_first(),
+                "date": self.clean_data(rate.xpath('th/text()').extract_first()),
+                "rate": self.clean_data(rate.xpath('td/text()').extract_first())
                 }
 
 
