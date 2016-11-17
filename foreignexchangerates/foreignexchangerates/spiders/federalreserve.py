ec6f29cb4d853c9209cbfb29333c7580ac2378e8
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-17 11:35:40 -0500

    Built out parse method for getting initial and handling data on secondary urls

--- foreignexchangerates/foreignexchangerates/spiders/federalreserve.py
+++ foreignexchangerates/foreignexchangerates/spiders/federalreserve.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class FederalreserveSpider(scrapy.Spider):
     name = "federalreserve"
     allowed_domains = ["federalreserve.gov"]
     start_urls = ['http://federalreserve.gov/']
 
     def parse(self, response):
-        pass
+        urls = list(map(response.urljoin, response.xpath('//table[@class="statistics"]/tbody/tr/th/a/@href')))
+        if urls == None:
+            for rate in response.xpath('//table[@class="statistics"]/tr'):
+                yield {
+                    "country": response.xpath('//h3/text()'),
+                    "date": rate.xpath('//th/text()'),
+                    "rate": rate.xpath('//td/text()')
+                }
