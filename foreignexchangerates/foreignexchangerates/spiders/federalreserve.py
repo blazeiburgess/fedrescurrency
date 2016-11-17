5052a112c07e66ced7792ea5d21b3a9402e14ac5
blaze.i.burgess <blaze.i.burgess@gmail.com>
2016-11-17 11:40:12 -0500

    Add conditional to traverse urls when appropriate

--- foreignexchangerates/foreignexchangerates/spiders/federalreserve.py
+++ foreignexchangerates/foreignexchangerates/spiders/federalreserve.py
@@ -8,10 +8,13 @@ class FederalreserveSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = list(map(response.urljoin, response.xpath('//table[@class="statistics"]/tbody/tr/th/a/@href')))
-        if urls == None:
+        if len(urls) == 0:
             for rate in response.xpath('//table[@class="statistics"]/tr'):
                 yield {
                     "country": response.xpath('//h3/text()'),
                     "date": rate.xpath('//th/text()'),
                     "rate": rate.xpath('//td/text()')
                 }
+        else:
+            for url in urls:
+                yield scrapy.Request(url, callback=self.parse)
